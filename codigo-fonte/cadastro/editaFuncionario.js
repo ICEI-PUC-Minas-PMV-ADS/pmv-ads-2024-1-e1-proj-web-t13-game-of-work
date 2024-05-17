83246e3533a435531d4c5ec31f4d74f339bab420
Nikolas <nikolasvictortola@gmail.com>
2024-05-17 19:49:08 -0300

    Editar funcionário (Sem a parte do Back-end)

--- codigo-fonte/cadastro/editaFuncionario.js
+++ codigo-fonte/cadastro/editaFuncionario.js
@@ -8,12 +8,31 @@ const botao__naoExcluirFuncionario = modal__excluirFunc.querySelector('.botao__n
 
 const email__excluirFuncionario = document.querySelector('.email__excluirFuncionario');
 
+
+
+
+const botao__editFunc = document.querySelector('.botao__editFunc');
+
+const modal__editFunc = document.querySelector('.modal__editFunc');
+const botao__fecharEditarFuncionario = modal__editFunc.querySelector('.botaoFechar');
+
+const botao__editar = document.querySelector('.botao__editar');
+
+
+const nomeFuncionario_edit = document.querySelector('#nomeFuncionario_edit');
+const emailFuncionario_edit = document.querySelector('#emailFuncionario_edit');
+const cargoFuncionario_edit = document.querySelector('#cargoFuncionario_edit');
+const input__especialidade = document.querySelector('#input__especialidade');
+const input__objetivos = document.querySelector('#input__objetivos');
+const input__sobreMim = document.querySelector('#input__sobreMim');
+const listaInput__softSkills = document.getElementsByClassName('input__softSkill');
+const listaInput__hardSkills = document.getElementsByClassName('input__hardSkill');
+
 botao__excluirFunc.addEventListener('click', function(){
 
     caixa__exibicao.innerText = '';
 
     let funcionarios = JSON.parse(localStorage.getItem('listaUsuarios'));
-    console.log(funcionarios);
 
     funcionarios.forEach(funcionario => {
         
@@ -64,6 +83,7 @@ botao__excluirFunc.addEventListener('click', function(){
 
 overlay.addEventListener('click', function(){
     modal__excluirFunc.setAttribute('style', 'display: none');
+    modal__editFunc.setAttribute('style', 'display: none');
 
 });
 
@@ -80,4 +100,87 @@ botao__simExcluirFuncionario.addEventListener('click', function(){
 botao__fecharExcluirFuncionario.addEventListener('click', function(){
     modal__excluirFunc.setAttribute('style', 'display:none');
     overlay.setAttribute('style', 'display:none');
+});
+
+botao__fecharEditarFuncionario.addEventListener('click', function(){
+    modal__editFunc.setAttribute('style', 'display:none');
+    overlay.setAttribute('style', 'display:none');
+});
+
+
+
+botao__editFunc.addEventListener('click', function(){
+
+    caixa__exibicao.innerText = '';
+
+    let funcionarios = JSON.parse(localStorage.getItem('listaUsuarios'));
+
+    funcionarios.forEach(funcionario => {
+
+        const emailFuncionario_edit = document.createElement('h3');
+        emailFuncionario_edit.innerHTML = funcionario.email;
+        emailFuncionario_edit.classList.add('funcionarioExibido');
+
+        caixa__exibicao.appendChild(emailFuncionario_edit);
+    });
+
+    let funcionariosListados = document.getElementsByClassName('funcionarioExibido');
+
+    for (let i = 0; i < funcionariosListados.length; i++) {
+        
+        funcionariosListados[i].addEventListener('click', function(e){
+
+            const funcionarioClicado = e.target;
+            var temClasse = false;
+
+            for (let i = 0; i < funcionarioClicado.classList.length; i++) {
+                if(funcionarioClicado.classList[i] == 'funcionarioSelecionado_edit') {
+                    temClasse = true;   
+                }
+            }
+
+            
+             if (temClasse) {
+                modal__editFunc.setAttribute('style', 'display:block');
+                overlay.setAttribute('style', 'display: block');
+
+                funcionarios.forEach(funcionario =>{
+
+                    if (funcionarioClicado.innerHTML == funcionario.email) {
+                        
+                        nomeFuncionario_edit.innerHTML = funcionario.nome;
+                        cargoFuncionario_edit.innerHTML = funcionario.cargo;
+                        emailFuncionario_edit.innerHTML = funcionario.email;
+
+                        input__especialidade.value = funcionario.especialidade;
+                        input__sobreMim.value = funcionarios.sobreMim;
+                        input__objetivos.value = funcionarios.input__objetivos;
+
+                        for (let i = 0; i < listaInput__softSkills.length; i++) {
+                            
+                            listaInput__softSkills[i].value = funcionario.softSkills[i];
+                
+                        }
+                        for (let i = 0; i < listaInput__hardSkills.length; i++) {
+                            
+                            listaInput__hardSkills[i].value = funcionario.hardSkills[i];
+
+                        }
+                    }
+                });
+        
+            } else{
+        
+                let funcionarioComClasse = document.querySelector('.funcionarioSelecionado_edit');
+
+                if (funcionarioComClasse) {
+                    funcionarioComClasse.classList.remove('funcionarioSelecionado_edit');
+                    funcionarioClicado.classList.toggle('funcionarioSelecionado_edit');
+            
+                } else
+                    funcionarioClicado.classList.toggle('funcionarioSelecionado_edit');
+        
+            }
+        });
+    }
 });
