2dd564ec74af5982608d3b1e6ee4f45bf752de8e
Nikolas <nikolasvictortola@gmail.com>
2024-05-23 14:48:55 -0300

    Ajustes para a apresentação do dia 23/05/2024

--- codigo-fonte/telaPerfil/script_tela_perfil.js
+++ codigo-fonte/telaPerfil/script_tela_perfil.js
@@ -1,5 +1,5 @@
-localStorage.setItem('nome', 'Catyane');
-localStorage.setItem('cargo', 'Desenvolvedor');
+localStorage.setItem('nome', 'Hayala');
+localStorage.setItem('cargo', 'Gestor');
 
 const nome_usuario = document.querySelector('#nome_usuario');
 const cargo_usuario = document.querySelector('#cargo_usuario');
@@ -15,14 +15,14 @@ nome_usuario.innerHTML = nome;
 cargo_usuario.innerHTML = cargo;
 
 if (cargo == 'Gestor') {
-    avatar.src = 'img/Avatar.png';
+    avatar.src = '../imagens/avatar/Avatar.png';
 }
 if (cargo == 'RH') {
-    avatar.src = 'img/RH.png'
+    avatar.src = '../imagens/avatar/RH.png'
 }
 if (cargo == 'Desenvolvedor') {
-    avatar.src = 'img/Desenvolvedor.png'
+    avatar.src = '../imagens/avatar/Desenvolvedor.png'
 }
 if (cargo == 'Design') {
-    avatar.src = 'img/Design.png'
+    avatar.src = '../imagens/avatar/Design.png'
 }
