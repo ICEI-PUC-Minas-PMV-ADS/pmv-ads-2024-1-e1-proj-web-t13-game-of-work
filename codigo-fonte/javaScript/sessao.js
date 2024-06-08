acec6f99bebb7c484959f8046cb4aff2367059d4
BrendaDrumond <165854427+BrendaDrumond@users.noreply.github.com>
2024-06-08 19:02:46 -0300

    Add files via upload

---
+++ codigo-fonte/javaScript/sessao.js
@@ -0,0 +1,23 @@
+let _nome = localStorage.getItem('nome');
+let _cargo = localStorage.getItem('cargo');
+let _email = localStorage.getItem('email');
+let _token = localStorage.getItem('token');
+
+var _sair = document.querySelector('#sair');
+
+_sair.addEventListener("click", function(){
+    localStorage.removeItem('nome');
+    localStorage.removeItem('cargo');
+    localStorage.removeItem('email');
+    localStorage.removeItem('token');
+
+})
+
+let secao = localStorage.getItem('token');
+
+if (secao==null) {
+    alert("Seção Expirada");
+    window.location.replace("../Login/telalogin.html");
+}
+
+
